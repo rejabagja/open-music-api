9cb77d0b50cd5db2b7f5a8385c5379281c523619
rejabagja <rezasubagja5@gmail.com>
2025-08-05 11:45:30 +0700

    feat(albums): add albums plugin

---
+++ migrations/1754365620961_create-albums-table.js
@@ -0,0 +1,32 @@
+/* eslint-disable camelcase */
+
+const up = (pgm) => {
+  pgm.createTable('albums', {
+    id: {
+      type: 'CHAR(22)',
+      primaryKey: true,
+    },
+    name: {
+      type: 'VARCHAR(100)',
+      notNull: true,
+    },
+    year: {
+      type: 'SMALLINT',
+      notNull: true,
+    },
+    created_at: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    updated_at: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+  });
+};
+
+const down = (pgm) => {
+  pgm.dropTable('albums');
+};
+
+module.exports = { up, down };
--- src/server.js
+++ src/server.js
@@ -3,8 +3,13 @@
 require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
+const ClientError = require('./exceptions/ClientError');
+const albums = require('./api/albums');
+const AlbumsValidator = require('./validator/albums');
+const AlbumsService = require('./services/postgres/AlbumsService');
 
 const init = async () => {
+  const albumsService = new AlbumsService();
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -14,6 +19,32 @@ const init = async () => {
       },
     },
   });
+
+  await server.register([
+    {
+      plugin: albums,
+      options: {
+        service: albumsService,
+        validator: AlbumsValidator,
+      },
+    },
+  ]);
+
+  server.ext('onPreResponse', (request, h) => {
+    const { response } = request;
+
+    if (response instanceof ClientError) {
+      const newResponse = h.response({
+        status: 'fail',
+        message: response.message,
+      });
+      newResponse.code(response.statusCode);
+      return newResponse;
+    }
+
+    return h.continue;
+  });
+
   await server.start();
   console.log('Server running on %s', server.info.uri);
 };
